62e201617cc64d0a8f401f25b240db783da12164
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-24 13:57:25 +0700

    Rilis renovasi

--- composition.js
+++ composition.js
@@ -2,34 +2,46 @@
 var fs = require('fs');
 
 //your code here
+class Ingredient {
+	constructor(options) {
+		this.name = options['name'];
+		this.amount = options['amount'];
+		this.has_sugar = options['has_sugar'];
+	}
+
+	// Extra
+}
+
 class Cookie{
-	constructor() {
-		this.status = "mentah"
+	constructor(name, ingredients) {
+		this.name = name;
+		this.status = "mentah";
+		this.ingredients = ingredients;
 	}
 
 	bake() {
-		this.status = "selesai dimasak"
+		this.status = "selesai dimasak";
 	}
 }
 
 class PeanutButter extends Cookie {
-	constructor() {
-		super();
+	constructor(name, ingredients) {
+		super(name, ingredients);
 		this.peanut_count = 100;
 	}
 }
 
 class ChocolateChip extends Cookie {
-	constructor() {
-		super();
-		this.choc_chip_count = 200
+	constructor(name, ingredients) {
+		super(name, ingredients);
+		this.choc_chip_count = 200;
 	}
 }
 
 class OtherCookie extends Cookie {
-	constructor() {
-		super();
-		this.other_count = 50
+	constructor(name, ingredients) {
+		super(name, ingredients);
+		this.other_count = 150;
 	}
 }
 
@@ -40,26 +52,64 @@ class CookieFactory {
 
 	static create(options) {
 		let arrCookieFactory = [];
-		let cookieData = fs.readFileSync(options.file);
+		let cookieData = fs.readFileSync('cookies.txt');
 		let arrCookies = cookieData.toString().split('\n');
+
+		// console.log(arrCookies);
 
 		for (var i = 0; i < arrCookies.length; i++) {
 			let cookieName = arrCookies[i].split(' ');
 
-			if (cookieName[0] === 'peanut') {
-				arrCookieFactory.push(new PeanutButter(arrCookies[i]));
+			if (cookieName[0] === 'peanut' && cookieName[1] === 'butter') {
+				let optPeanutCookie = [];
+				optPeanutCookie.push(new Ingredient({ name: 'flour', amount: '1 cup', has_sugar: false }));
+				optPeanutCookie.push(new Ingredient({ name: 'sugar', amount: '2 cups (gluten)', has_sugar: true }));
+				optPeanutCookie.push(new Ingredient({ name: 'peanut butter', amount: '2 cups', has_sugar: false }));
+				optPeanutCookie.push(new Ingredient({ name: 'butter', amount: '2 tsp', has_sugar: false }));
+
+				arrCookieFactory.push(new PeanutButter(arrCookies[i], optPeanutCookie));
 			} else if (cookieName[0] === 'chocolate' && cookieName[1] === 'chip') {
-				arrCookieFactory.push(new ChocolateChip(arrCookies[i]));
+				let optChocolateCookie = [];
+				optChocolateCookie.push(new Ingredient({ name: 'chip', amount: '1 cup', has_sugar: false }));
+				optChocolateCookie.push(new Ingredient({ name: 'sugar', amount: '1 cups', has_sugar: true }));
+				optChocolateCookie.push(new Ingredient({ name: 'butter', amount: '2 tsp', has_sugar: false }));
+
+				arrCookieFactory.push(new ChocolateChip(arrCookies[i], optChocolateCookie));
 			} else {
-				arrCookieFactory.push(new OtherCookie(arrCookies[i]));
+				let optOtherCookie = [];
+				optOtherCookie.push(new Ingredient({ name: 'flour', amount: '1 cup', has_sugar: false }));
+				optOtherCookie.push(new Ingredient({ name: 'sugar', amount: '1 cups', has_sugar: true }));
+				optOtherCookie.push(new Ingredient({ name: 'cinnamon', amount: '2 tsp', has_sugar: false }));
+
+				arrCookieFactory.push(new OtherCookie(arrCookies[i], optOtherCookie));
 			}
 		}
 
-		return arrCookieFactory;
+
+		// console.log(arrCookieFactory);
+		let strCokieTxt = "";
+
+		// for (var j = 0; j < 3; j++) {
+		// 	strCokieTxt +=  arrCookieFactory[j].name+" = "+CookieFactory.ingredientToString(arrCookieFactory[j].ingredients)+"\n";		
+		// }
+
+		// console.log(strCokieTxt);
+		// fs.writeFileSync('cookie.txt', strCokieTxt, 'utf-8');
+
+		return strCokieTxt;
+	}
+
+	static ingredientToString(ingredients) {
+		let strIngredients = '';
+
+		for (var i = 0; i < ingredients.length; i++) {
+			strIngredients += `${ingredients[i].name} : ${ingredients[i].amount} `;
+		}
+
+		return strIngredients;
 	}
 }
 
-let options = {};
-options.file = 'cookies.txt';
-let batch_of_cookies = CookieFactory.create(options);
+let allCookies = [];
+let batch_of_cookies = CookieFactory.create(allCookies);
 console.log(batch_of_cookies);
